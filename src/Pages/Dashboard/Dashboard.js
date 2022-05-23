1cb4743b53d67ef106a034c47cc417b5a354e82f
abidhossain2 <tanvirabid229@gmail.com>
2022-05-24 01:13:51 +0600

    nested link create

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import Menubar from '../Menubar/Menubar';
-
+import './Dashboard.css'
 
 const Dashboard = () => {
+
     return (
         <>
             <div className='bg-black'>
                 <Menubar></Menubar>
             </div>
-            <div>
-                    <Link to=''>My Orders</Link>
-                    <Link to='addreview'>Add Review</Link>
-                    <Link to='myprofile'>My Profile</Link>
+            <div className='nested-link-container'>
+                    <NavLink to='/dashboard' className='nested-link'>My Orders</NavLink>
+                    <NavLink to='addreview' className='nested-link'>Add Review</NavLink>
+                    <NavLink to='myprofile' className='nested-link'>My Profile</NavLink>
             </div>
             <div>
                 <Outlet/>
