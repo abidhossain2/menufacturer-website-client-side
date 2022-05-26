04c713dfc305b72cc79ef3cf34cbd4c5c0109fc4
abidhossain2 <tanvirabid229@gmail.com>
2022-05-26 10:39:45 +0600

    delete user function create

--- src/Pages/Dashboard/AllUser/AllUserTable.js
+++ src/Pages/Dashboard/AllUser/AllUserTable.js
@@ -1,31 +1,60 @@
-import React from 'react';
-import { toast} from 'react-toastify';
+import React, { useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 
 const AllUserTable = ({ singleUser, refetch }) => {
-    const { email, role } = singleUser;
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const { email, role, _id } = singleUser;
     const handleAdmin = () => {
         fetch(`http://localhost:5000/adminuser/${email}`, {
             method: 'PUT'
         })
-        .then(res => res.json())
-        .then(data => {
-            if(data.modifiedCount > 0){
-                toast.success('Successfully created admin')
-                refetch();
-            }
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    toast.success('Successfully created admin')
+                    refetch();
+                }
+            })
+    }
+    const handleDelete = () => {
+        fetch(`http://localhost:5000/adminuser/${_id}`, {
+            method: 'DELETE'
         })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                toast.success('Successfully Deleted')
+                refetch();
+                setShow(false)
+            })
     }
     return (
-        <div className='user-table'>
-            {role === 'admin' ? <p> <span className='fw-bold'>Admin:</span> {email}</p> : <p><span className='fw-bold'>User: </span>{email}</p>}
-            <div className='user-table-btn'> {role !== 'admin' &&
-                <>
-                    <button className='makeadmin-btn' onClick={handleAdmin}>Make Admin</button>
-                    <button className='deleteuser-btn'>Delete User</button>
-                </>
-            }</div>
+        <>
+            <Modal show={show} onHide={handleClose}>
+                <div className='modal-txt'>Are you sure to delete?</div>
+                <div className='modal-body'>
+                    <button className='modal-btn-success' onClick={() => handleDelete(_id)}>
+                        Yes
+                    </button>
+                    <button className='modal-btn-false' onClick={handleClose}>
+                        No
+                    </button>
+                </div>
+            </Modal>
+            <div className='user-table'>
+                {role === 'admin' ? <p> <span className='fw-bold'>Admin:</span> {email}</p> : <p><span className='fw-bold'>User: </span>{email}</p>}
+                <div className='user-table-btn'> {role !== 'admin' &&
+                    <>
+                        <button className='makeadmin-btn' onClick={handleAdmin}>Make Admin</button>
+                        <button className='deleteuser-btn' onClick={handleShow}>Delete User</button>
+                    </>
+                }</div>
 
-        </div>
+            </div>
+        </>
     );
 };
 
